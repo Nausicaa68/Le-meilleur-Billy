87592c532976522a5f954a42e6c11022cb43886e
Guipas68 <58084848+Nausicaa68@users.noreply.github.com>
2022-07-03 02:53:21 +0200

    Update test.py

--- test.py
+++ test.py
@@ -19,11 +19,21 @@ caractObjet = {
 
 
 count = 0
+counter2 = 0
 print(count)
+print(counter2)
 for key1 in caractObjet:
-        for key2 in caractObjet:
-            for key3 in caractObjet:
-                if((key1 != key2) and (key1 != key3) and (key2 != key3)):
-                    # construire un Billy tout neuf
-                    count += 1
+    for key2 in caractObjet:
+        for key3 in caractObjet:
+            if((key1 != key2) and (key1 != key3) and (key2 != key3)):
+                count += 1
+                #print(count, ":", key1, key2, key3)
+
+                if(key1 == "epee" or key1 == "lance" or key1 == "arc"):
+                    if(key2 == "epee" or key2 == "lance" or key2 == "arc"):
+                        if(key3 == "epee" or key3 == "lance" or key3 == "arc"):
+                            print(count, ":", key1, key2, key3)
+                            counter2 += 1
+
 print(count)
+print(counter2)
